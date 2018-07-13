480e47265abcc254fbd096191c12c8cd31020eb8
adeelzhang <adeelzhang@webank.com>
2018-07-13 14:36:26 +0800

    V2版本修改

--- src/main/java/demo/springboot/web/HelloController.java
+++ src/main/java/demo/springboot/web/HelloController.java
@@ -16,6 +16,6 @@ public class HelloController {
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     @ResponseBody
     public String sayHello() {
-        return "Hello，Spring Boot";
+        return "V2 Hello Spring Boot";
     }
 }
